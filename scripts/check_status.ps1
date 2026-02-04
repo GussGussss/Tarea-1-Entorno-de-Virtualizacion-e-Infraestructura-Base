9046a46f320009a1b97c10d5ce0e18d550b78872
GussGussss <gustavopacheco3005@gmail.com>
2026-02-04 00:23:30 -0700

    Delete scripts directory

--- scripts/check_status.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "*************** SCRIPT DE BIENVENIDA **************"
-$nombre=hostname
-$ip=Get-NetIPAddress -AddressFamily IPv4
-$disco=Get-Volume -DriveLetter C | Format-Table -Autosize | Out-String
-Write-Host "`nNombre de equipo: $nombre"
-Write-Host "`nIP del equipo: $ip"
-Write-Host "`nEspacio en el disco: $disco"
--- scripts/check_status.sh
+++
@@ -1,7 +0,0 @@
-echo "*********** SCRIPT DE BIENVENIDA ***********"
-nombre=$(hostname)
-ip=$(hostname -I)
-espacio=$(df -h /)
-echo -e "\nNombre del equipo: $nombre"
-echo -e "\IP del equipo: $ip"
-echo -e "\nEspacio en el disco: \n$espacio\n"
